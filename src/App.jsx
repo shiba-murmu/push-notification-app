5104468f191c5990bed8bce5598c2029aa5ff572
Shiba Murmu <shibamurmu001@gmail.com>
2025-08-31 08:53:56 +0530

    animation added in the login page . Use custom css to animation to changing the text animated way.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import React, { useRef } from "react";
-
+import Sign_in from "./pages/Sign_in";
 function App() {
     const [theme, setTheme] = useState(
         localStorage.theme || "system" // default to system
@@ -63,7 +63,7 @@ function App() {
         return (
             <>
                 {/* <!-- ====== Dropdowns Section Start --> */}
-                <section className="bg-[#ffffff] dark:bg-[#02021e] border-b-1 border-[#c8c8c8] dark:border-[#2d2f38]  py-3 dark:bg-dark">
+                <section className="bg-[#ffffff] dark:bg-[var(--color-bg-dark)] border-b-1 border-[#c8c8c8] dark:border-[#2d2f38]  py-3 dark:bg-dark">
                     <div className="container">
                         <div className="flex justify-center">
                             {/* one */}
@@ -72,7 +72,7 @@ function App() {
                                     <div className="relative inline-block text-left">
                                         <button
                                             onClick={() => setDropdownOpen(!dropdownOpen)}
-                                            className={`bg-[#efefef]  dark:bg-[#1f2a37] hover:dark:bg-[#293749] flex md:w-50 items-center rounded-[5px] px-5 py-[13px] border border-[#c9c7c7] dark:border-[#2d2f38] text-base font-medium text-[#787878] dark:text-white`}
+                                            className={`bg-[#efefef]  dark:bg-[var(--color-bg-light-dark)] hover:dark:bg-[#293749] flex md:w-50 items-center rounded-[5px] px-5 py-[13px] border border-[#c9c7c7] dark:border-[#2d2f38] text-base font-medium text-[#787878] dark:text-white`}
                                         >
                                             Change theme
                                             <span className="pl-4">
@@ -127,6 +127,7 @@ function App() {
 
             <div className="h-screen  bg-white dark:bg-gray-900 text-black dark:text-white transition-colors">
                 <Dropdown />
+                <Sign_in />
             </div>
             <div className="text-center p-4 bg-gray-200 dark:bg-[#02021e] text-gray-800 dark:text-gray-200">
                 Footer
